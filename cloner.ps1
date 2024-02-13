223f536e49ee3b021d2b1d0dbc4bd39fd9b378fe
Sean Barrick <71162295+seabar24@users.noreply.github.com>
2024-02-12 23:36:22 -0500

    Update cloner.ps1

--- cloner.ps1
+++ cloner.ps1
@@ -6,8 +6,8 @@ Get-VM
 $vmname = Read-Host -prompt "Enter the name of the VM you wish to make a Linked Clone of"
 $vm=Get-VM -Name $vmname
 $snapshot = Get-Snapshot -VM $vm -Name "Base"
-$vmhost = Read-Host -prompt "Enter the name of your VM Host Server"
-$vmhost = Get-VMHost -Name $vmhost
+$hostsname = Read-Host -prompt "Enter the name of your VM Host Server"
+$vmhost = Get-VMHost -Name $hostsname
 $dsname = "Enter the name of the Datastore you want to use"
 $ds=Get-DataStore -Name $dsname
 $linkedName = "{0}.linked" -f $vm.name
@@ -16,6 +16,6 @@ $linkedvm = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $sn
 #Create the Full VM
 $newvm = New-VM -Name "server.2019.base.v2" -VM $linkedvm -VMHost $vmhost -Datastore $ds
 #A new Snap Shot
-$newvm | new-snapshot -Name "Base"
+$newvm | New-Snapshot -Name "Base"
 #Cleanup the temporary linked clone
 $linkedvm | Remove-VM
