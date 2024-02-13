980507d70a7ec1bf3af2a11d39189caaace4ad51
Sean Barrick <71162295+seabar24@users.noreply.github.com>
2024-02-12 23:32:10 -0500

    Update cloner.ps1

--- cloner.ps1
+++ cloner.ps1
@@ -1,14 +1,15 @@
 #Connect
-$vserver=Read-Host -prompt "Enter your vCenter Server (i.e vcenter.yourname.local) "
+$vserver=Read-Host -prompt "Enter your vCenter Server (i.e vcenter.yourname.local)"
 Connect-VIServer($vserver)
 #Source VM
 Get-VM
-$vmname = Read-Host -prompt "Enter the name of the VM you wish to make a Linked Clone of "
+$vmname = Read-Host -prompt "Enter the name of the VM you wish to make a Linked Clone of"
 $vm=Get-VM -Name $vmname
 $snapshot = Get-Snapshot -VM $vm -Name "Base"
-$vmhost = Read-Host -prompt "Enter the name of your VM Host Server: "
+$vmhost = Read-Host -prompt "Enter the name of your VM Host Server"
 $vmhost = Get-VMHost -Name $vmhost
-$ds=Get-DataStore -Name BASEVM
+$dsname = "Enter the name of the Datastore you want to use"
+$ds=Get-DataStore -Name $dsname
 $linkedName = "{0}.linked" -f $vm.name
 #Create the tempory VM
 $linkedvm = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
