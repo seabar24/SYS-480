6663e141d4b04b8ae89e2db553897a0869f35554
sbarrick <sbarrick@xubuntu-wan.sean.local>
2024-02-28 23:44:04 -0500

    90% complete w/ Milestone 5

--- modules/480-utils/480-utils.psm1
+++ modules/480-utils/480-utils.psm1
@@ -53,6 +53,9 @@ Function Get-480Config([string] $config_path)
 
 Function Select-VM([string] $folder)
 {
+    # This function will get a list of VMs from the vm_folder specified in 480.json
+    # And gets an input from the user to select a VM to be used for a Linked Clone
+    Write-Host "Select your VM:"
     $selected_vm=$null
     try
     {
@@ -74,4 +77,66 @@ Function Select-VM([string] $folder)
     {
         Write-Host "invalid folder: $folder" -ForegroundColor "Red" 
     }
+}
+
+Function Select-DB()
+{
+   # This function will get all the datastores associated with your vcenter
+   # And returns the chosen Datastore from user choice
+   Write-Host "Select your Datastore:"
+   $chosen_db=$null
+   try 
+   {
+        $datastores = Get-Datastore
+        $index = 1
+        foreach($ds in $datastores)
+        {
+            Write-Host [$index] $ds.Name
+            $index+=1
+        }
+        $choice = Read-Host "Which index number [x] do you wish to pick?"
+        $chosen_db = $datastores[$choice -1]
+        Write-Host "You picked " $chosen_db.Name
+        return $chosen_db
+   }
+   catch 
+   {
+    # Fix this/don't need this anymore
+        Write-Host "Invalid datastore: $database" -ForegroundColor "Red"
+   }
+}
+Function Select-Network($vm)
+{
+    # This function gets the network from your vcenter
+    # And returns the chosen network for the full clone
+    Write-Host "Select your Network Adapter:"
+    $chosen_net=$null
+    
+    $networks = Get-NetworkAdapter -VM $vm
+    $index=1
+    foreach($net in $networks)
+    {
+        Write-Host [$index] $net.Name
+        $index+=1
+    }
+    $choice = Read-Host "Which index number [x] do you wish to pick?"
+    $chosen_net = $networks | Select-Object -Index ($choice - 1) | Select-Object -ExpandProperty Name
+    Write-Host "You picked " $chosen_net.Name
+    return $chosen_net  
+}
+
+Function FullClone([string] $vm, $snap, $vmhost, $ds, $network)
+{
+    # This function creates a new linked clone vm temporarily
+    # And returns a newly created vm from the linked clone
+    $linkedName = "{0}.linked" -f $vm
+    # Creates a temp. linked clone
+    $linkedVM = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snap -VMHost $vmhost -Datastore $ds
+    # Gets name of new Full Clone and creates it from linked clone
+    $newvmname = Read-Host -prompt "Enter the name for your New VM"
+    $newVM = New-VM -Name $newvmname -VM $linkedVM -VMHost $vmhost -Datastore $ds
+    $newVM | Set-NetworkAdapter -NetworkName $network
+    # Creates a new snapshot called "Base" and removes the temp.
+    $newVM | New-Snapshot -Name "Base"
+    $linkedVM | Remove-VM
 }
--- modules/480-utils/480driver.ps1
+++ modules/480-utils/480driver.ps1
@@ -1,7 +1,16 @@
 Import-Module "480-utils" -Force
 # Call the Banner Function
 480Banner 
-$config = Get-480Config -config_path "/home/sbarrick/modules/480-utils/480.json"
+$config = Get-480Config -config_path "/home/sbarrick/SYS-480/modules/480-utils/480.json"
+
 480Connect -server $config.vcenter_server
-Write-Host "Selecting you VM"
-Select-VM -folder "BASEVM"
+
+$vm = Select-VM -folder $config.vm_folder
+
+$db = Select-DB
+
+$snapshot = Get-Snapshot -VM $vm.Name | Select-Object -First 1
+
+$network = Select-Network -vm $vm.Name
+
+FullClone -vm $vm.Name -snap $snapshot -vmhost $config.esxi_host -ds $db -network $network
